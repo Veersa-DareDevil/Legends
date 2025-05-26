d27422e30c7757b436e657d5c048703c8ae3f224
snehil <snehil16work@gmail.com>
2025-05-27 01:01:52 +0530

    api

--- src/pageObject/api/commonFunctions.ts
+++ src/pageObject/api/commonFunctions.ts
@@ -1,13 +1,13 @@
 export class CommonFunctions {
   static buildQueryString(
-    params: Record<string, string | number | boolean | (string | number | boolean)[]>
+    params: Record<string, string | number | boolean | (string | number | boolean)[]>,
   ): string {
     return Object.entries(params)
       .flatMap(([key, value]) =>
         Array.isArray(value)
           ? value.map((v) => `${key}=${encodeURIComponent(String(v))}`)
-          : `${key}=${encodeURIComponent(String(value))}`
+          : `${key}=${encodeURIComponent(String(value))}`,
       )
-      .join('&');
+      .join('&')
   }
 }

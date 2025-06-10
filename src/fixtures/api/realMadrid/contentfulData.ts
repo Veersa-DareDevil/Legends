2a8502caf8cb0ba0f790c2abca50a2c55fb5c225
snehil <snehil16work@gmail.com>
2025-06-10 16:17:41 +0530

    contentful

--- src/fixtures/api/realMadrid/contentfulData.ts
+++ src/fixtures/api/realMadrid/contentfulData.ts
@@ -1,7 +1,7 @@
 export const localEnglish = {
   code: 'en',
   fullCode: 'en-US',
-  value: 'Add to Cart',
+  value: 'Add to Cart - Test RM',
   description: 'English',
   original: 'Add to Cart',
 }
